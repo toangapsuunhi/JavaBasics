6978f6ec0496d29ee6818427b565312c09a90709
toangapsuunhi <vanthang152002@gmail.com>
2021-10-24 20:55:18 +0700

    Initial commit

---
+++ src/main/java/B02_datatypes_control_loop/Main_helloworld.java
@@ -0,0 +1,17 @@
+
+package B02_datatypes_control_loop;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main_helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
