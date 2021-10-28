c3dbb558b890edb5a6cb79efe811c0529e549a62
toangapsuunhi <vanthang152002@gmail.com>
2021-10-28 20:41:28 +0700

    Update Main_helloworld.java

--- src/main/java/B02_datatypes_control_loop/Main_helloworld.java
+++ src/main/java/B02_datatypes_control_loop/Main_helloworld.java
@@ -1,6 +1,8 @@
 
 package B02_datatypes_control_loop;
 
+import static java.awt.SystemColor.text;
+
 /**
  *
  * @author Admin
@@ -12,6 +14,44 @@ public class Main_helloworld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        int s = sum(2,3);
+        System.out.println(s);
+        System.out.println(sum(4,6));
+        
+        printToConsole("Some text");
+        
+        System.out.println("Factorial of 4: " + interativeFactorial(4));
+        
+        System.out.println("Factorial of 4: " + calculateFactorial(4));
+    }
+    private static int sum(int a, int b){
+        return a + b;
+    }
+
+    private static void printToConsole(String some_text) {
+       
+    System.out.println(some_text);
     }
     
-}
+    // calculateFactorial n*(n+1) ...2 1
+    private static int interativeFactorial(int n){
+    
+        int factorial = 1;
+       
+        for (int i = 1; i <= n; ++i){
+//            factorial = factorial * i;
+            factorial *=i;
+        }
+        return factorial;
+        }
+    private static int calculateFactorial(int n){
+        
+        if (n !=0){
+            return n * calculateFactorial(n - 1);
+        } else{
+            return 1;
+        }
+    }
+    }
+
